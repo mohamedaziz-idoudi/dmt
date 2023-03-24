b8b8323732bb56a0298dd74d1b0f87060fa7f881
Mohamed Aziz Idoudi <mohamedaziz.idoudi@etudiant-fst.utm.tn>
2023-03-25 00:17:02 +0100

    css

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -39,7 +39,7 @@ const Footer = () => {
                             <a href="https://tn.linkedin.com/company/digimytch?trk=public_profile_topcard-current-company" target="_blank" rel="noreferrer"><i className="icon ion-social-linkedin"></i></a>
                             <a href="#"><i className="icon ion-social-youtube"></i></a>
                             <a href="https://www.instagram.com/digimytch.tn/?hl=fr"target="_blank" rel="noreferrer"><i className="icon ion-social-instagram"></i></a>
-                            <a href="https://www.tiktok.com/@digimitch" target="_blank" rel="noreferrer"><react.Icon icon="ion:logo-tiktok" /></a>
+                            <a href="https://www.tiktok.com/@digimytch.com?_d=secCgYIASAHKAESPgo8YhDciGd7c36XWjE7tsXWcuKolhTUMdpUxdG47vfj56qInXjelwzGNte13W0JGoUj9Sziu67LilTU1VMyGgA%3D&checksum=ce1295612ee9eef9f4ff5cce3eff56b61438b7301ac0ecd580f074be6c92093a&language=fr&sec_uid=MS4wLjABAAAAV_BXKm21nrpRddPklBRh4gMgYoGkLexZiBingL69plV9tfU8KeMpoo4S_MVPKE4k&sec_user_id=MS4wLjABAAAAECNzoVnxANkfrYQt_WCQM0RPsZO1p-SKkI1bhpBcbgAgOx5k4dgeETmGBlVeh3Z3&share_app_id=1340&share_author_id=7204017771608212485&share_link_id=29af4068-7e74-4bd7-bbc4-d16390419a2b&timestamp=1678475985&u_code=dc6c018dj2e1j3&ugbiz_name=Account&user_id=6821662282637444101&utm_campaign=client_share&utm_source=copy&_r=1" target="_blank" rel="noreferrer"><react.Icon icon="ion:logo-tiktok" /></a>
                             <p className="copyright"><a href="https://www.facebook.com/profile.php?id=100085406739373" target="blank">© Created by ONCA Solution - 2022</a></p>
                         </div>
                     </div>
